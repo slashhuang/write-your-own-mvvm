b1150e0f6bdebd7bf5ff4b5229c2b4f6a45e0ca6
slashhuang <627284482@qq.com>
2016-10-27 00:24:25 +0800

    almost finish simple mvvm

--- index.js
+++ index.js
@@ -13,12 +13,32 @@ class mvvm {
         this.logic()
     }
     logic(){
-        this.parser(this.config);//解析模板
+        this.defineGetterSetter();
+        this.parser();//解析模板,生成虚拟dom
         this.addEvent(this.virtualDom); //挂载事件
         this.render();//渲染到dom节点
     }
-    parser(domConfig) {
-        let { template,data} =domConfig;
+    defineGetterSetter(){
+        let {data} = this.config;
+        let $data = data();
+        //定义简化版的getter setter
+        let self =this;
+        for(let key in $data){ //get set仍旧挂载在this.config上面
+            Object.defineProperty(this.config, key, {
+                set: function(newVal) {
+                    self.digest(key,newVal);
+                    return newVal
+                }
+            });
+        }
+    }
+    //更新dom
+    digest(key,val){
+        this.virtualDom.data[key] = val;
+        this.render();
+    }
+    parser() {
+        let { template,data} =this.config;
         //初始化的数据
         let $data =data();
         //存储dom节点对应的指令
@@ -36,12 +56,15 @@ class mvvm {
         });
         //解析dom节点的innerHTML对应的key值
         let keyName=musReg.exec(fragment.html())[1];
-        fragment.html($data[keyName]);
         //生成虚拟dom结构
         let fragVdom= {
             fragment:fragment, //dom
             cmdList:cmdList,  //指令
             data:$data,//数据
+            watcherKey:keyName,//依赖的键值数组
+            renderFunc:function(){ //渲染方式
+                return this.fragment.html(this.data[this.watcherKey]);
+            },
             children:[]
         };
         //这里只是个简单演示,并未做遍历dom结构处理
@@ -61,8 +84,8 @@ class mvvm {
         return ()=>methods[name].apply(this.config); //保证引用中的this指向config文件
     }
     render() {
-        let {fragment} = this.virtualDom;
-        this.target.empty().append($(fragment));
+        debugger;
+        this.target.empty().append(this.virtualDom.renderFunc());
     }
 };
 let config = {
